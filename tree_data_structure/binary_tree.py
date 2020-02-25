c891a5bd44442d36e18f572adfc3b1ef398a495e
sachinthakur9614 <sachin.thakur@mca.christuniversity.in>
2020-02-26 00:51:19 +0530

    added new fea.

--- tree_data_structure/binary_tree.py
+++ tree_data_structure/binary_tree.py
@@ -22,6 +22,30 @@ class Node:
         else:
             self.data = data
     
+    def delete_node(self,root,key):
+        if not root:
+            return root
+        if root.data >key:
+            root.left = self.delete_node(root.left,key)
+        elif root.data < key:
+            root.right = self.delete_node(root.right,key)
+        else:
+            if not root.right:
+                return root.left
+            if not root.left:
+                return root.right
+            
+            temp_val  = root.right
+            mini_val = temp_val.data
+            while temp_val.left:
+                temp_val = temp_val.left
+                mini_val =  temp_val.data
+            root.data = mini_val
+
+            root.right = self.delete_node(root.right,root.data)
+        return root
+
+    
     def find_val(self,key_val):
         if key_val<self.data:
             if self.left is None:
@@ -40,6 +64,25 @@ class Node:
         print(self.data)
         if self.right:
             self.right.PrintTree()
+    
+    def post_order(self,root):
+        if root:
+            self.post_order(root.left)
+            self.post_order(root.right)
+            print(root.data)
+    
+    def in_order(self,root):
+        if root:
+            self.in_order(root.left)
+            print(root.data)
+            self.in_order(root.right)
+
+    def pre_order(self,root):
+        if root:
+            print(root.data)
+            self.pre_order(self.left)
+            self.pre_order(self.right)
+
 
 
 root = Node(12)
